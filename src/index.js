bc259c0c061935032988639f85ab68c7ac2d7aa8
Your NameIlya <kirnichnyilya@gmail.com>
2018-09-11 03:20:54 +0300

    '🔥'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function warmup(temperature) {
-  // your implementation here
+module.exports = function warmup(celsius) {
+  // Долго со всем мучался, я просто совсем новичек
+  // Оказалось все проще чем я тут написывал))
+  // Спасибо YouTube
+  var fahrenheit;
+  fahrenheit=celsius * 9 / 5 + 32;
+  return fahrenheit;
+ 
 };
